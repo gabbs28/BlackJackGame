69029113014ab4772613e45267e3728c77b8c4fa
gabbs28 <gabbsruiz@outlook.com>
2023-11-25 23:55:11 -0800

    jh

---
+++ Deck.js
@@ -0,0 +1,12 @@
+class Deck{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    makeDeck() {
+        const deck = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "J", "Q", "K"];
+        const types = ["C", "H", "D", "S"];
+    };
+
+};
